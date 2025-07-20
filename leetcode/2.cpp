3882c445a05e0d5b743c938432fb6fb3faf38b4c
LULU <384668412@qq.com>
2025-07-20 17:14:07 +0800

    lc-2. addTwoNumbers

---
+++ leetcode/2.cpp
@@ -0,0 +1,34 @@
+#include "header.h"
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *head = new ListNode;
+        ListNode *tail = head;
+        int carry = 0;
+        while (l1 || l2)
+        {
+            int val1 = l1 ? l1->val : 0;
+            int val2 = l2 ? l2->val : 0;
+            tail->next = new ListNode((val1 + val2 + carry) % 10);
+            carry = (val1 + val2 + carry) / 10;
+            tail = tail->next;
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
+        }
+        if (carry == 1)
+            tail->next = new ListNode(1);
+        return head->next;
+    }
+};
--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
-#include "1.cpp"
+#include "2.cpp"
 
 int main()
 {
+    Solution Solution;
+    ListNode *l1 = new ListNode(9);
+    auto h1 = l1;
+    h1->next = new ListNode(9);
+    h1 = h1->next;
+    h1->next = new ListNode(9);
+    h1 = h1->next;
+    h1->next = new ListNode(9);
+    h1 = h1->next;
+    h1->next = new ListNode(9);
+    h1 = h1->next;
+    h1->next = new ListNode(9);
+    h1 = h1->next;
+    h1->next = new ListNode(9);
+    h1 = h1->next;
 
+    ListNode *l2 = new ListNode(9);
+    auto h2 = l2;
+    h2->next = new ListNode(9);
+    h2 = h2->next;
+    h2->next = new ListNode(9);
+    h2 = h2->next;
+    h2->next = new ListNode(9);
+    h2 = h2->next;
+
+    auto solution = Solution.addTwoNumbers(l1, l2);
+    while (solution)
+    {
+        cout << solution->val << endl;
+        solution = solution->next;
+    }
     return 0;
 }
