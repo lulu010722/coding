e6b5887c4f288b716e9c2b2eb279ec1240b130f7
LULU <384668412@qq.com>
2025-07-20 16:36:19 +0800

    lc-1. twoSum

---
+++ leetcode/1.cpp
@@ -0,0 +1,63 @@
+#include "header.h"
+
+class Solution
+{
+public:
+
+    // O(nlogn)
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        vector<int> solution;
+        vector<pair<int, int>> numsWithIndex;
+
+        for (int i = 0; i < n; i++)
+        {
+            numsWithIndex.push_back(make_pair(nums[i], i));
+        }
+
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+        int first = 0;
+        int second = n - 1;
+        while (first < second)
+        {
+            int firstValue = numsWithIndex[first].first;
+            int secondValue = numsWithIndex[second].first;
+            if (firstValue + secondValue < target)
+            {
+                first++;
+            }
+            else if (firstValue + secondValue > target)
+            {
+                second--;
+            }
+            else
+            {
+                solution.push_back(numsWithIndex[first].second);
+                solution.push_back(numsWithIndex[second].second);
+                break;
+            }
+        }
+
+        return solution;
+    }
+
+    // O(n^2)
+    // vector<int> twoSum(vector<int> &nums, int target)
+    // {
+    //     int n = nums.size();
+    //     vector<int> solution;
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         for (int j = i + 1; j < n; j++)
+    //         {
+    //             if (nums[i] + nums[j] == target)
+    //             {
+    //                 solution.push_back(i);
+    //                 solution.push_back(j);
+    //             }
+    //         }
+    //     }
+    //     return solution;
+    // }
+};
---
+++ leetcode/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "1.cpp"
+
+int main()
+{
+
+    return 0;
+}
