e590d1c466512bdd2aef3461cad505fc53de0dbc
LULU <384668412@qq.com>
2025-07-27 19:17:27 +0800

    lc-20. isValid

---
+++ leetcode/20.cpp
@@ -0,0 +1,31 @@
+#include "header.h"
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> stack;
+        for (auto c : s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else if (c == ')')
+                if (stack.empty() || stack.top() != '(')
+                    return false;
+                else
+                    stack.pop();
+            else if (c == ']')
+                if (stack.empty() || stack.top() != '[')
+                    return false;
+                else
+                    stack.pop();
+            else if (c == '}')
+                if (stack.empty() || stack.top() != '{')
+                    return false;
+                else
+                    stack.pop();
+        }
+        return stack.empty();
+    }
+};
