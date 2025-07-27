e18e0ac03c5394f85018b6f1938ecae98e0f7ef9
LULU <384668412@qq.com>
2025-07-27 17:10:56 +0800

    lc-16. threeSumClosest

---
+++ leetcode/16.cpp
@@ -0,0 +1,33 @@
+#include "header.h"
+
+class Solution
+{
+public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int minDiff = INT_MAX;
+        int result = target;
+
+        for (int i = 0; i < n; i++)
+        {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right)
+            {
+                int diff = nums[i] + nums[left] + nums[right] - target;
+                if (abs(diff) < minDiff)
+                {
+                    minDiff = abs(diff);
+                    result = nums[i] + nums[left] + nums[right];
+                }
+                if (diff < 0)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return result;
+    }
+};
