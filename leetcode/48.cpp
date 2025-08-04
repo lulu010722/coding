f47a2a4d8997742151ccf7cc3f762834547570cb
LULU <384668412@qq.com>
2025-08-04 18:49:09 +0800

    lc-48. rotate

---
+++ leetcode/48.cpp
@@ -0,0 +1,16 @@
+#include "header.h"
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        vector<vector<int>> result(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                result[i][j] = matrix[n - 1 - j][i];
+        
+        copy(result.begin(), result.end(), matrix.begin());
+    }
+};
